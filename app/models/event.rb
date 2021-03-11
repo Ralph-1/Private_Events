5c5d3aff1165670ccfb37846b7fab68802ee73b9
Ralph <ralphoburu@gmail.com>
2021-03-12 01:54:24 +0300

    Add events table with associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: 'User'
+
+  validates :date, presence: true
+  validates :description, presence: true
 end
--- db/migrate/20210311224154_create_events.rb
+++ db/migrate/20210311224154_create_events.rb
@@ -1,6 +1,7 @@
 class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
+      t.string :name
       t.date :date
       t.string :description
 
